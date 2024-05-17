da3898e9096c167ffbdfbf6d5ef7f08ae32bf481
VasudevanS1906 <157589360+VasudevanS1906@users.noreply.github.com>
2024-05-17 08:47:13 +0530

    Create main.java

---
+++ src/main/java/com/example/main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        System.out.println("2 + 3 = " + result);
+    }
+}
